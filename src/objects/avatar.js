303e684ea108925a1385e2cbc5aad4eabc994ff9
kknapp <kknapp@redhat.com>
2019-07-04 17:54:27 -0600

    Added game design and project roadmap docs. Added example world / sprite scenes.

---
+++ src/objects/avatar.js
@@ -0,0 +1,11 @@
+const avatar = {
+	types: {
+		warrior: {}, 	// physical power
+		guardian: {}, 	// defense and protection
+		genius: {},		// cooldown manipulation
+		druid: {},		// adaptability, provides smaller bonuses for a wider variety of options
+		goblin: {},		// better rewards, more risks
+		shaman: {},		// utility, buffs, debuffs
+	},
+	
+};
